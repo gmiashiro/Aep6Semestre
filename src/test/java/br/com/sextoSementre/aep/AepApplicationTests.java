fa01031ae389c14f2764a3fa13330d9379e3c1fc
gmiashiro <g.miashiro.freitas@gmail.com>
2026-08-26 13:09:39 -0300

    Adicionando projeto base

---
+++ src/test/java/br/com/sextoSementre/aep/AepApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.sextoSementre.aep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class AepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
